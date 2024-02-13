1938068b3f4ad8b9d528863d6f0722301a06c765
MBATheGamer <mbathegamer@gmail.com>
2024-02-13 14:37:02 +0100

    assignment

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
+<script lang="ts">
+  import { defineNuxtComponent } from '#app';
+import type { Todo } from './types';
+
+  export default defineNuxtComponent({
+    data: () => ({
+      todos: [] as Todo[]
+    }),
+    methods: {
+      fetchTodos() {
+        fetch("https://jsonplaceholder.typicode.com/todos/")
+          .then(response => response.json())
+          .then(json => {
+            this.todos = json as Todo[];
+          });
+      }
+    }
+  })
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Todos</h1>
+    <button @click="fetchTodos">Fetch todos</button>
+    <ul>
+      <li v-for="todo in todos" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed"> {{ todo.title }}
+      </li>
+    </ul>
   </div>
 </template>
