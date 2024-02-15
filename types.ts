5a71edc215db2d95ceb34e1657a95d3ee0a4a77f
MBATheGamer <mbathegamer@gmail.com>
2024-02-15 17:25:09 +0100

    Update types

--- types.ts
+++ types.ts
@@ -4,3 +4,11 @@ export type Todo = {
   title: string,
   completed: boolean
 }
+
+export type Photo = {
+  albumId: number,
+  id: number,
+  title: string,
+  url: string,
+  thumbnailUrl: string
+}
